16e879dff8933f5c2209eebafc3aa0e4e13da534
Felipe Johnny <felipijohnny@outlook.com>
2025-02-22 12:05:26 -0300

    aplicando telad e gerenciar sistema para administradores

--- src/app/[empresaTag]/protect/gerenciar-sistema/page.tsx
+++ src/app/[empresaTag]/protect/gerenciar-sistema/page.tsx
@@ -1,18 +1,15 @@
 // app/[empresa]/protect/home/page.tsx
 
-import LogoutButton from "@/components/logoutButton/logoutButton";
-import NavComponent from "@/components/nav/nav";
 import ScreenValidator from "@/components/ScreenValidator/ScreenValidator";
 
 export default function EmpresaHomePage() {
   return (
     <main className="p-6">
-      {/* Valida se o usuário tem acesso à tela "Home" */}
+      {/* Valida se o usuário tem acesso à tela "Gerenciar Sistema" */}
       <ScreenValidator screenName="Gerenciar Sistema" />
-      <h1>Tela Gerenciar</h1>
-      <NavComponent />
-
-      <LogoutButton />
+      <div className="dark:border-primary-600/70 border-primary-300 flex h-[90dvh] flex-wrap rounded-3xl border-2 bg-transparent p-5 backdrop-blur-2xl dark:border-1 dark:bg-black/20 dark:backdrop-blur-2xl">
+        <h1>gerenciar</h1>
+      </div>
     </main>
   );
 }
