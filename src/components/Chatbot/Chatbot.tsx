a104ec45404659739dccf5da7a1e562186fb3fed
Felipe Johnny <felipijohnny@outlook.com>
2025-02-26 08:06:16 -0300

    aplicando ajustes de codigo

--- src/components/Chatbot/Chatbot.tsx
+++ src/components/Chatbot/Chatbot.tsx
@@ -1,15 +1,15 @@
 "use client";
-import React, { useState, useRef, useEffect } from 'react';
-import { Form } from '../form';
-import { Send } from 'lucide-react';
-import NavBarMenuChat from './NavBarMenuChat';
-import ToggleTheme from '../button/ToggleTheme';
-import { fluxo, setores } from './setores';
+import React, { useState, useRef, useEffect } from "react";
+import { Form } from "../form";
+import { Send } from "lucide-react";
+import NavBarMenuChat from "./NavBarMenuChat";
+import ToggleTheme from "@/components/ui/button/ToggleTheme";
+import { fluxo, setores } from "./setores";
 
 type Message = {
   text: string;
   time: string;
-  type: 'user' | 'bot';
+  type: "user" | "bot";
   loading?: boolean;
 };
 
@@ -17,13 +17,13 @@ export default function Chatbot() {
   const [messages, setMessages] = useState<Message[]>([
     {
       text: fluxo[0].pergunta,
-      time: new Date().toLocaleTimeString('pt-BR', {
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit'
+      time: new Date().toLocaleTimeString("pt-BR", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
       }),
-      type: 'bot'
-    }
+      type: "bot",
+    },
   ]);
   const [title, setTitle] = useState<string>(fluxo[0].title);
   const [messageUser, setMessageUser] = useState("");
@@ -32,23 +32,27 @@ export default function Chatbot() {
   const messagesEndRef = useRef<HTMLDivElement>(null);
   const [formDataChamados, setFormDataChamados] = useState<any>({});
   const [countdown, setCountdown] = useState<number | null>(null);
-  
+
   // Variável para armazenar as mensagens enviadas pelo usuário
   const [dataUserChamados, setDataUserChamados] = useState<string[]>([]);
 
-  const sendMessage = (newMessage: string, type: 'user' | 'bot', loading = false) => {
-    setMessages(prevMessages => [
+  const sendMessage = (
+    newMessage: string,
+    type: "user" | "bot",
+    loading = false,
+  ) => {
+    setMessages((prevMessages) => [
       ...prevMessages,
       {
         text: newMessage,
-        time: new Date().toLocaleTimeString('pt-BR', {
-          hour: '2-digit',
-          minute: '2-digit',
-          second: '2-digit'
+        time: new Date().toLocaleTimeString("pt-BR", {
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
         }),
         type,
-        loading
-      }
+        loading,
+      },
     ]);
   };
 
@@ -77,14 +81,21 @@ export default function Chatbot() {
           messages,
           setor: setorSelecionado,
           title,
-          dataUserChamados // Aqui você inclui os dados enviados pelo usuário
+          dataUserChamados, // Aqui você inclui os dados enviados pelo usuário
         };
         setFormDataChamados(dadosChamado);
-        sendMessage("Finalizando...", 'bot');
+        sendMessage("Finalizando...", "bot");
         setCountdown(5);
       }, 3500);
     }
-  }, [etapaAtual, countdown, messages, setorSelecionado, title, dataUserChamados]);
+  }, [
+    etapaAtual,
+    countdown,
+    messages,
+    setorSelecionado,
+    title,
+    dataUserChamados,
+  ]);
 
   // Função genérica para enviar mensagem com tratamento especial para "Voltar"
   const handleSendMessage = (text?: string) => {
@@ -92,24 +103,24 @@ export default function Chatbot() {
     if (!messageToSend.trim()) return;
 
     if (messageToSend === "Descrição") {
-      sendMessage(messageToSend, 'user');
+      sendMessage(messageToSend, "user");
       setMessageUser("");
       // Adiciona uma mensagem de loading
-      sendMessage("Escrevendo...", 'bot', true);
+      sendMessage("Escrevendo...", "bot", true);
       setTimeout(() => {
-        setMessages(prev => {
+        setMessages((prev) => {
           const updated = [...prev];
-          const indexLoading = updated.findIndex(msg => msg.loading);
+          const indexLoading = updated.findIndex((msg) => msg.loading);
           if (indexLoading !== -1) {
             updated[indexLoading] = {
               ...updated[indexLoading],
               text: fluxo[0].pergunta, // Retorna para a descrição inicial
               loading: false,
-              time: new Date().toLocaleTimeString('pt-BR', {
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit'
-              })
+              time: new Date().toLocaleTimeString("pt-BR", {
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+              }),
             };
             // Reseta o fluxo para o início
             setEtapaAtual(0);
@@ -124,31 +135,31 @@ export default function Chatbot() {
 
     // Fluxo normal:
     // Salva a mensagem enviada pelo usuário em dataUserChamados
-    setDataUserChamados(prevData => [...prevData, messageToSend]);
+    setDataUserChamados((prevData) => [...prevData, messageToSend]);
 
-    sendMessage(messageToSend, 'user');
+    sendMessage(messageToSend, "user");
     setMessageUser("");
 
     const respostaUsuario = messageToSend;
     const proximaEtapa = fluxo[etapaAtual].next(respostaUsuario);
 
-    sendMessage("Escrevendo...", 'bot', true);
+    sendMessage("Escrevendo...", "bot", true);
 
     setTimeout(() => {
-      setMessages(prev => {
+      setMessages((prev) => {
         const updated = [...prev];
-        const indexLoading = updated.findIndex(msg => msg.loading);
+        const indexLoading = updated.findIndex((msg) => msg.loading);
         if (indexLoading !== -1) {
           if (proximaEtapa !== null) {
             updated[indexLoading] = {
               ...updated[indexLoading],
               text: fluxo[proximaEtapa].pergunta,
               loading: false,
-              time: new Date().toLocaleTimeString('pt-BR', {
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit'
-              })
+              time: new Date().toLocaleTimeString("pt-BR", {
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+              }),
             };
             setEtapaAtual(proximaEtapa);
             setTitle(fluxo[proximaEtapa].title);
@@ -157,11 +168,11 @@ export default function Chatbot() {
               ...updated[indexLoading],
               text: "Obrigado pelas informações!",
               loading: false,
-              time: new Date().toLocaleTimeString('pt-BR', {
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit'
-              })
+              time: new Date().toLocaleTimeString("pt-BR", {
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+              }),
             };
             setTitle("");
           }
@@ -179,16 +190,16 @@ export default function Chatbot() {
 
   useEffect(() => {
     // Scroll automático para o final da lista de mensagens
-    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
   return (
-    <div className="w-[800px] h-[600px] flex flex-col items-center relative">
-      <div className="flex w-full h-full dark:bg-primary-900 ">
+    <div className="relative flex h-[600px] w-[800px] flex-col items-center">
+      <div className="dark:bg-primary-900 flex h-full w-full">
         {/* Menu lateral: Se etapaAtual for 1, exibe os setores; se for 2, exibe os motivos do setor selecionado */}
         {etapaAtual === 1 && (
-          <NavBarMenuChat className="p-1.5 w-[500px] h-full bg-blue-100 dark:bg-primary-900 border-[0.5px] border-blue-500 dark:border-amber-50/60 rounded-l-[15px] border-r-0">
-            <h1 className="text-lg font-semibold text-gray-900 dark:text-gray-100 text-[1.5rem] text-center p-3">
+          <NavBarMenuChat className="dark:bg-primary-900 h-full w-[500px] rounded-l-[15px] border-[0.5px] border-r-0 border-blue-500 bg-blue-100 p-1.5 dark:border-amber-50/60">
+            <h1 className="p-3 text-center text-lg text-[1.5rem] font-semibold text-gray-900 dark:text-gray-100">
               {title}
             </h1>
             <div className="flex flex-col gap-1.5">
@@ -205,8 +216,8 @@ export default function Chatbot() {
           </NavBarMenuChat>
         )}
         {etapaAtual === 2 && setorSelecionado && (
-          <NavBarMenuChat className="p-3 w-[500px] h-full bg-blue-100 dark:bg-primary-900 border-[0.5px] border-blue-500 dark:border-amber-50/60 rounded-l-[15px] border-r-0">
-            <h1 className="text-lg font-semibold text-primary-100 dark:text-gray-100">
+          <NavBarMenuChat className="dark:bg-primary-900 h-full w-[500px] rounded-l-[15px] border-[0.5px] border-r-0 border-blue-500 bg-blue-100 p-3 dark:border-amber-50/60">
+            <h1 className="text-primary-100 text-lg font-semibold dark:text-gray-100">
               {title}
             </h1>
             <div className="flex flex-col gap-1.5">
@@ -223,48 +234,60 @@ export default function Chatbot() {
           </NavBarMenuChat>
         )}
 
-        <div className="flex flex-col w-full h-full bg-blue-100 dark:bg-primary-900 border-[0.5px] border-blue-500 dark:border-amber-50/60 rounded-[15px]">
+        <div className="dark:bg-primary-900 flex h-full w-full flex-col rounded-[15px] border-[0.5px] border-blue-500 bg-blue-100 dark:border-amber-50/60">
           <header className="p-4">
-            <h1 className="text-lg font-semibold text-gray-900 dark:text-gray-100 text-[1rem]">
+            <h1 className="text-lg text-[1rem] font-semibold text-gray-900 dark:text-gray-100">
               {title}
             </h1>
           </header>
-          <div className="flex-1 p-4 overflow-y-scroll space-y-4 relative">
+          <div className="relative flex-1 space-y-4 overflow-y-scroll p-4">
             {messages.map((msg, index) => (
               <Form.Paragrafo
                 key={index}
                 className={
                   msg.loading
                     ? ""
-                    : `relative p-6 flex pl-15 rounded-tl-2xl shadow-md gap-1 break-words w-[400px] border border-amber-50 ${
-                        msg.type === 'user'
-                          ? 'text-wrap bg-blue-500 text-white ml-auto flex justify-end pl-[15px] rounded-r-[15px] rounded-br-[50px]'
-                          : 'animate-move-right-to-left bg-linear-to-r/srgb from-primary-100 to-primary-150 text-amber-50 rounded-r-[15px] rounded-bl-[50px] rounded-[15px]'
+                    : `relative flex w-[400px] gap-1 rounded-tl-2xl border border-amber-50 p-6 pl-15 break-words shadow-md ${
+                        msg.type === "user"
+                          ? "ml-auto flex justify-end rounded-r-[15px] rounded-br-[50px] bg-blue-500 pl-[15px] text-wrap text-white"
+                          : "animate-move-right-to-left from-primary-100 to-primary-150 rounded-[15px] rounded-r-[15px] rounded-bl-[50px] bg-linear-to-r/srgb text-amber-50"
                       }`
                 }
               >
-                {msg.type === 'bot' && (
+                {msg.type === "bot" && (
                   <img
-                    className={msg.loading ? "hidden" : "absolute -top-2 -left-0 h-14 w-14 rounded-full border-2 border-amber-50"}
+                    className={
+                      msg.loading
+                        ? "hidden"
+                        : "absolute -top-2 -left-0 h-14 w-14 rounded-full border-2 border-amber-50"
+                    }
                     src="/image/chatAmalfis/amalfiszinho.png"
                     alt="amalfiszinho"
                   />
                 )}
-                <span className={msg.loading ? "animate-pulse" : "text-wrap max-w-[350px]"}>
+                <span
+                  className={
+                    msg.loading ? "animate-pulse" : "max-w-[350px] text-wrap"
+                  }
+                >
                   {msg.text}
                   {etapaAtual === 4 && index === messages.length - 1 && (
-                    <div className="p-5 flex gap-2">
+                    <div className="flex gap-2 p-5">
                       <button
-                        className="bg-red-500 p-3 rounded-[15px] w-[100px]"
+                        className="w-[100px] rounded-[15px] bg-red-500 p-3"
                         value="Descrição"
-                        onClick={(e) => handleSendMessage(e.currentTarget.value)}
+                        onClick={(e) =>
+                          handleSendMessage(e.currentTarget.value)
+                        }
                       >
                         Voltar
                       </button>
                       <button
-                        className="bg-green-500 p-3 rounded-[15px] w-[100px]"
+                        className="w-[100px] rounded-[15px] bg-green-500 p-3"
                         value="Finalizar"
-                        onClick={(e) => handleSendMessage(e.currentTarget.value)}
+                        onClick={(e) =>
+                          handleSendMessage(e.currentTarget.value)
+                        }
                         onKeyDown={(e) => {
                           if (e.key === "Enter") {
                             handleSendMessage();
@@ -276,7 +299,7 @@ export default function Chatbot() {
                     </div>
                   )}
                 </span>
-                <span className="absolute bottom-1 right-7 text-sm text-amber-50">
+                <span className="absolute right-7 bottom-1 text-sm text-amber-50">
                   {msg.loading ? "" : msg.time}
                 </span>
               </Form.Paragrafo>
@@ -288,7 +311,7 @@ export default function Chatbot() {
             <div className="p-3">
               <div className="flex gap-4">
                 <input
-                  className="flex-1 rounded-2xl border-blue-500 dark:border-primary-700 p-1 bg-white dark:bg-amber-50 dark:text-gray-900"
+                  className="dark:border-primary-700 flex-1 rounded-2xl border-blue-500 bg-white p-1 dark:bg-amber-50 dark:text-gray-900"
                   value={messageUser}
                   type="text"
                   placeholder="Digite sua mensagem"
@@ -302,7 +325,7 @@ export default function Chatbot() {
                 />
                 <Send
                   tabIndex={0}
-                  className="bg-primary-100 dark:bg-primary-200 w-[50px] h-[35px] p-1 rounded-[10px] cursor-pointer"
+                  className="bg-primary-100 dark:bg-primary-200 h-[35px] w-[50px] cursor-pointer rounded-[10px] p-1"
                   onClick={() => handleSendMessage()}
                   onKeyDown={(e) => {
                     if (e.key === "Enter") {
@@ -315,9 +338,9 @@ export default function Chatbot() {
           )}
           {etapaAtual === 3 && (
             <div className="p-3">
-              <div className="flex gap-4 items-center">
+              <div className="flex items-center gap-4">
                 <textarea
-                  className="flex-1 rounded-2xl border-blue-500 dark:border-primary-700 p-1 bg-white dark:bg-amber-50 dark:text-gray-900"
+                  className="dark:border-primary-700 flex-1 rounded-2xl border-blue-500 bg-white p-1 dark:bg-amber-50 dark:text-gray-900"
                   value={messageUser}
                   placeholder="Digite sua mensagem"
                   onChange={(e) => setMessageUser(e.target.value)}
@@ -328,7 +351,7 @@ export default function Chatbot() {
                   }}
                 />
                 <Send
-                  className="bg-primary-100 dark:bg-primary-200 w-[50px] h-[35px] p-1 rounded-[10px] cursor-pointer"
+                  className="bg-primary-100 dark:bg-primary-200 h-[35px] w-[50px] cursor-pointer rounded-[10px] p-1"
                   onClick={() => handleSendMessage()}
                 />
               </div>
@@ -339,7 +362,7 @@ export default function Chatbot() {
       <ToggleTheme />
       {/* Overlay para o contador de finalização */}
       {countdown !== null && (
-        <div className="fixed inset-0 flex items-center justify-center dark:bg-primary-300">
+        <div className="dark:bg-primary-300 fixed inset-0 flex items-center justify-center">
           <h2 className="text-4xl text-white">Finalizando em {countdown}...</h2>
         </div>
       )}
