b86cd692529b5afef7609ae00df24ca31205125d
Felipe Johnny <felipijohnny@outlook.com>
2025-02-26 20:15:52 -0300

    aplicando ajuste

--- src/components/form/FormLogo.tsx
+++ src/components/form/FormLogo.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 type LogoFormProps = React.ComponentProps<"div"> & {
   src: string;
   alt: string;
@@ -8,11 +6,10 @@ type LogoFormProps = React.ComponentProps<"div"> & {
 export default function FormLogo({ src, alt, ...props }: LogoFormProps) {
   return (
     <div {...props}>
-    
       <img
         src={src}
         alt={alt}
-        className=" justify-self-center items-center h-15 "
+        className="h-15 items-center justify-self-center"
       />
     </div>
   );
