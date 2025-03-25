0b5f4772f4a1b7fe0868d729d19c86c8ce7e7bc4
otaviosouza21 <otaviosouzalu@gmail.com>
2025-03-24 23:34:51 -0300

    ajustes

--- src/actions/postUser.ts
+++ src/actions/postUser.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
  "use server";
  import apiError from "@/functions/api-error";
  import { cookies } from "next/headers";
@@ -29,90 +28,6 @@
    const permissionsCheckbox = formData.getAll("checkbox[]") as string[] | null;
    const permissionsObject= permissionsCheckbox?.map((permission)=> JSON.parse(permission)) 
    const permissionsComplete = permissionsObject?.map(permission=>{
-=======
-"use server";
-
-import apiError from "@/functions/api-error";
-import { cookies } from "next/headers";
-import jwt from "jsonwebtoken";
-import {
-  PermissaoCompletaData,
-  PermissaoUserData,
-  TokenData,
-} from "@/types/api/apiTypes";
-import logout from "./logout";
-import { POST_USUARIO } from "@/functions/api";
-import { revalidateTag } from "next/cache";
-import { redirect } from "next/dist/server/api-utils";
-import { access } from "node:fs";
-
-// Defina explicit amente o tipo de retorno esperado
-export async function postUser(
-  state:
-    | { errors: string[]; msg_success: string; success: boolean }
-    | undefined,
-  formData: FormData,
-): Promise<{ errors: string[]; msg_success: string; success: boolean }> {
-  const nome = formData.get("nome") as string;
-  const contato = formData.get("contato") as string;
-  const email = formData.get("email") as string;
-  const senha = formData.get("senha") as string;
-  const tipoUsuario = formData.get("tipo_usuario") as string;
-  const permissionsCheckbox = formData.getAll("checkbox[]") as string[] | null;
-  const errors: string[] = [];
-  const permissions: PermissaoUserData[] = [];
-  permissionsCheckbox?.map((permission) => {
-    const permissionsData: PermissaoCompletaData & any = JSON.parse(permission);
-    const permTeste = JSON.parse(permission);
-    const newPermission = {
-      permissao_id: permissionsData.id,
-      nome: permissionsData.nome,
-      acessos: {
-        can_create: permissionsData.crud.create,
-        can_read: permissionsData.crud.read,
-        can_update: permissionsData.crud.update,
-        can_delete: permissionsData.crud.delete,
-      },
-      acoes: [],
-    };
-
-    permissions.push(newPermission);
-  });
-
-  console.log(permissions);
-  if (errors.length > 0) {
-    return { errors, msg_success: "", success: false };
-  }
-
-  try {
-    const token = (await cookies()).get("token")?.value;
-    if (!token) {
-      return { errors, msg_success: "", success: false };
-    }
-    const usuarioData = jwt.decode(token) as TokenData;
-
-    const { url } = await POST_USUARIO();
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        Authorization: "Bearer " + token,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        nome: nome,
-        email: email,
-        contato: contato,
-        senha: senha,
-        roles_id: [tipoUsuario],
-        permissoes: permissions,
-        empresa_id: usuarioData.empresa.id,
-      }),
-    });
-
-    if (response.ok) {
-      const json = await response.json();
-      revalidateTag("new-user");
->>>>>>> 72b914a81e2e9f6044e8b4971c4fa9300306c60d
       return {
         permissao_id: permission.screen_id,
         acessos:{
--- src/components/formSteps/FormStepsUser.tsx
+++ src/components/formSteps/FormStepsUser.tsx
@@ -1,223 +1,136 @@
- "use client";
+"use client";
 
- import React, { useActionState, useEffect } from "react";
- import { Form } from "../form";
- import { toast } from "react-toastify";
- import { redirect } from "next/navigation";
- import RegisterNavigation from "../nav/registersNavigation/RegisterNavigation";
- import {
-   Check,
-   ChevronLeft,
-   ChevronRight,
-   KeyRound,
-   MailIcon,
-   Phone,
-   UserCircle,
- } from "lucide-react";
- import { RoleData, PermissaoCompletaData } from "@/types/api/apiTypes";
- import PermissionsMenu from "./permissionsMenu/PermissionsMenu";
- import { postUser } from "@/actions/postUser";
- import { useGlobalContext } from "@/context/globalContext";
+import React, { useActionState, useEffect } from "react";
+import { Form } from "../form";
+import { toast } from "react-toastify";
+import { redirect } from "next/navigation";
+import RegisterNavigation from "../nav/registersNavigation/RegisterNavigation";
+import {
+  Check,
+  ChevronLeft,
+  ChevronRight,
+  KeyRound,
+  MailIcon,
+  Phone,
+  UserCircle,
+} from "lucide-react";
+import { RoleData, PermissaoCompletaData } from "@/types/api/apiTypes";
+import PermissionsMenu from "./permissionsMenu/PermissionsMenu";
+import { postUser } from "@/actions/postUser";
+import { useGlobalContext } from "@/context/globalContext";
 
- type FormStepsUser = {
-   rolesData: RoleData[];
-   permissoesData: PermissaoCompletaData[];
- };
+type FormStepsUser = {
+  rolesData: RoleData[];
+  permissoesData: PermissaoCompletaData[];
+};
 
- export default function FormStepsUser({
-   rolesData,
-   permissoesData,
- }: FormStepsUser) {
-   const [activeTab, setActiveTab] = React.useState("informacoes");
-   const [currentRoles, setCurrentRoles] = React.useState<RoleData[] | []>(
-     rolesData,
-   );
-   const { openGlobalModal, closeGlobalModal } = useGlobalContext();
-   const [state, formAction] = useActionState(postUser, {
-     errors: [],
-     msg_success: "",
-     success: false,
-   });
+export default function FormStepsUser({
+  rolesData,
+  permissoesData,
+}: FormStepsUser) {
+  const [activeTab, setActiveTab] = React.useState("informacoes");
+  const [currentRoles, setCurrentRoles] = React.useState<RoleData[] | []>(rolesData);
+  const { openGlobalModal, closeGlobalModal } = useGlobalContext();
+  const [state, formAction] = useActionState(postUser, {
+    errors: [],
+    msg_success: "",
+    success: false,
+  });
 
-   useEffect(() => {
-     if (state?.errors.length) {
-       state.errors.forEach((erro: string) => {
-         toast.error(erro);
-       });
-     }
-     if (state?.success) {
-       toast.success(state.msg_success);
-       closeGlobalModal();
-       redirect("/devagile/protect/gerenciar-sistema/usuarios-do-sistema");
-     }
-   }, [state]);
+  useEffect(() => {
+    if (state?.errors.length) {
+      state.errors.forEach((erro: string) => {
+        toast.error(erro);
+      });
+    }
+    if (state?.success) {
+      toast.success(state.msg_success);
+      closeGlobalModal();
+      redirect("/devagile/protect/gerenciar-sistema/usuarios-do-sistema");
+    }
+  }, [state]);
 
-   return (
-     <div className="flex gap-5">
-       <RegisterNavigation setActiveTab={setActiveTab} activeTab={activeTab} />
-       <Form.Root action={formAction}>
-           <div className={`${activeTab !== 'informacoes' ? 'hidden' : ''}`}>
-             <Form.Section title="Dados do usuario">
-               <Form.InputText
-                 icon={UserCircle}
-                 inputId="nome"
-                 name="nome"
-                 label="Nome"
-               />
-               <Form.InputText
-                 icon={Phone}
-                 inputId="contato"
-                 name="contato"
-                 label="Contato"
-               />
-             </Form.Section>
-             <Form.Section title="Acesso ao sistema">
-               <Form.InputText
-                 icon={MailIcon}
-                 inputId="email"
-                 type="mail"
-                 name="email"
-                 label="Email"
-               />
-               <Form.InputText
-                 icon={KeyRound}
-                 inputId="senha"
-                 type="password"
-                 name="senha"
-                 label="Senha"
-               />
-               <Form.InputText
-                 icon={KeyRound}
-                 inputId="confirmar_senha"
-                 type="password"
-                 name="senha"
-                 label="Confirmar Senha"
-               />
-             </Form.Section>
-             <Form.ButtonNext
-               type="submit"
-               onClick={(e) => {
-                 e.preventDefault();
-                 setActiveTab("permissoes");
-               }}
-               direction="Continuar"
-               icon={ChevronRight}
-             />
-           </div>
-           <div className={`${activeTab !== 'permissoes' ? 'hidden' : ''}`}>
-             <Form.Section title="Perfil">
-               <Form.InputSelect
-                 options={currentRoles}
-                 label="Tipo de usuario"
-                 id="tipo_usuario"
-                 name="tipo_usuario"
-               />
-             </Form.Section>
-             <Form.Section title="Permissões">
-               <PermissionsMenu permissoesData={permissoesData} />
-             </Form.Section>
-             <div className="flex justify-between">
-               <Form.ButtonBack
-                 type="button"
-                 onClick={(e) => {
-                   e.preventDefault();
-                   setActiveTab("informacoes");
-                 }}
-                 direction="Voltar"
-                 icon={ChevronLeft}
-               />
-               <Form.ButtonNext
-                 type="submit"
-                 direction="Finalizar"
-                 icon={Check}
-               />
-             </div>
-           </div>
-       </Form.Root>
-     </div>
-   );
- }
   return (
     <div className="flex gap-5">
       <RegisterNavigation setActiveTab={setActiveTab} activeTab={activeTab} />
       <Form.Root action={formAction}>
-          <div className={`${activeTab !== 'informacoes' ? 'hidden' : ''}`}>
-            <Form.Section title="Dados do usuario">
-              <Form.InputText
-                icon={UserCircle}
-                inputId="nome"
-                name="nome"
-                label="Nome"
-              />
-              <Form.InputText
-                icon={Phone}
-                inputId="contato"
-                name="contato"
-                label="Contato"
-              />
-            </Form.Section>
-            <Form.Section title="Acesso ao sistema">
-              <Form.InputText
-                icon={MailIcon}
-                inputId="email"
-                type="mail"
-                name="email"
-                label="Email"
-              />
-              <Form.InputText
-                icon={KeyRound}
-                inputId="senha"
-                type="password"
-                name="senha"
-                label="Senha"
-              />
-              <Form.InputText
-                icon={KeyRound}
-                inputId="confirmar_senha"
-                type="password"
-                name="senha"
-                label="Confirmar Senha"
-              />
-            </Form.Section>
-            <Form.ButtonNext
-              type="submit"
+        <div className={`${activeTab !== 'informacoes' ? 'hidden' : ''}`}>
+          <Form.Section title="Dados do usuario">
+            <Form.InputText
+              icon={UserCircle}
+              inputId="nome"
+              name="nome"
+              label="Nome"
+            />
+            <Form.InputText
+              icon={Phone}
+              inputId="contato"
+              name="contato"
+              label="Contato"
+            />
+          </Form.Section>
+          <Form.Section title="Acesso ao sistema">
+            <Form.InputText
+              icon={MailIcon}
+              inputId="email"
+              type="mail"
+              name="email"
+              label="Email"
+            />
+            <Form.InputText
+              icon={KeyRound}
+              inputId="senha"
+              type="password"
+              name="senha"
+              label="Senha"
+            />
+            <Form.InputText
+              icon={KeyRound}
+              inputId="confirmar_senha"
+              type="password"
+              name="senha"
+              label="Confirmar Senha"
+            />
+          </Form.Section>
+          <Form.ButtonNext
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              setActiveTab("permissoes");
+            }}
+            direction="Continuar"
+            icon={ChevronRight}
+          />
+        </div>
+        <div className={`${activeTab !== 'permissoes' ? 'hidden' : ''}`}>
+          <Form.Section title="Perfil">
+            <Form.InputSelect
+              options={currentRoles}
+              label="Tipo de usuario"
+              id="tipo_usuario"
+              name="tipo_usuario"
+            />
+          </Form.Section>
+          <Form.Section title="Permissões">
+            <PermissionsMenu permissoesData={permissoesData} />
+          </Form.Section>
+          <div className="flex justify-between">
+            <Form.ButtonBack
+              type="button"
               onClick={(e) => {
                 e.preventDefault();
-                setActiveTab("permissoes");
+                setActiveTab("informacoes");
               }}
-              direction="Continuar"
-              icon={ChevronRight}
+              direction="Voltar"
+              icon={ChevronLeft}
+            />
+            <Form.ButtonNext
+              type="submit"
+              direction="Finalizar"
+              icon={Check}
             />
           </div>
-          <div className={`${activeTab !== 'permissoes' ? 'hidden' : ''}`}>
-            <Form.Section title="Perfil">
-              <Form.InputSelect
-                options={currentRoles}
-                label="Tipo de usuario"
-                id="tipo_usuario"
-                name="tipo_usuario"
-              />
-            </Form.Section>
-            <Form.Section title="Permissões">
-              <PermissionsMenu permissoesData={permissoesData} />
-            </Form.Section>
-            <div className="flex justify-between">
-              <Form.ButtonBack
-                type="button"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setActiveTab("informacoes");
-                }}
-                direction="Voltar"
-                icon={ChevronLeft}
-              />
-              <Form.ButtonNext
-                type="submit"
-                direction="Finalizar"
-                icon={Check}
-              />
-            </div>
-          </div>
+        </div>
       </Form.Root>
     </div>
   );
