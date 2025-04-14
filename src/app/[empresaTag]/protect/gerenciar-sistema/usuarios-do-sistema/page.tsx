0c283c00405d448e8af9ab50a574ef6577e8e91e
otaviosouza21 <otaviosouzalu@gmail.com>
2025-04-13 23:43:39 -0300

    Implementando filtros complexos na listagem de usuarios

--- src/app/[empresaTag]/protect/gerenciar-sistema/usuarios-do-sistema/page.tsx
+++ src/app/[empresaTag]/protect/gerenciar-sistema/usuarios-do-sistema/page.tsx
@@ -2,6 +2,7 @@ import getUsersByEmpresaId from "@/actions/getUsersByEmpresaId";
 import { validateScreenAccess } from "@/actions/validateScreenAccess";
 import UsuariosContainer from "@/components/Usuarios/UsuariosContainer";
 
+
 export default async function UsuariosDoSistemaPage() {
   await validateScreenAccess("Usuarios do sistema");
   const { data, ok, error } = await getUsersByEmpresaId();
